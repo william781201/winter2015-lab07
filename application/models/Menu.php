1022bf1cc699b6050a4f767b69d323761f17e3a7
lockan <lockan@gmail.com>
2015-03-24 15:50:24 -0700

    Fix for Menu.php

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -18,12 +18,12 @@ class Menu extends CI_Model {
         $this->xml = simplexml_load_file(DATAPATH . 'menu.xml');
 
         // build the list of patties - approach 1
-        foreach ($xml->patties->patty as $patty) {
+        foreach ($this->$xml->patties->patty as $patty) {
             $patty_names[(string) $patty['code']] = (string) $patty;
         }
 
         // build a full list of patties - approach 2
-        foreach ($xml->patties->patty as $patty) {
+        foreach ($this->$xml->patties->patty as $patty) {
             $record = new stdClass();
             $record->code = (string) $patty['code'];
             $record->name = (string) $patty;
@@ -40,7 +40,7 @@ class Menu extends CI_Model {
     // retrieve a patty record, perhaps for pricing
     function getPatty($code) {
         if (isset($this->patties[$code]))
-            return $this->patty['code'];
+            return $this->patties[$code];
         else
             return null;
     }
