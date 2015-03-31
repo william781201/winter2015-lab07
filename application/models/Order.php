686caf78275145eff4ab9c3be793125d768fd864
William <william781201@gmail.com>
2015-03-30 22:37:24 -0700

    fix order Signed-off-by:William <william781201@gmail.com>

--- application/models/Order.php
+++ application/models/Order.php
@@ -12,127 +12,200 @@
  * @author william
  */
 class Order extends CI_Model {
-    public $burgers = array();
+    
     protected $xml = null;
-    public $customer;
-    public $type;
-    public $instructions;
-    public $total;
-    public $filename;
+    protected $orders = Array();
     
     // Constructor
-    public function __construct($filename) {
+    public function __construct()
+    {
         parent::__construct();
-        if ($filename == null)
-        {
-            return;
-        }
-        $burgerCount = 0;
         
-        $this->load->model('Menu');
-        $this->xml = simplexml_load_file(DATAPATH . $filename);
-        
-        // Retrieve name and order type
-        $this->customer = (string) $this->xml->customer;
-        $this->type = (string) $this->xml['type'];
-        
-        // Assign the order instructions
-        if (isset($this->xml->instructions)) {
-            $this->instructions = (string) $this->xml->instructions;
-        }
-            
-            
-        foreach ($this->xml->burger as $burger) 
-        {
-            $burgerCount++;
-            $burger['num'] = $burgerCount;
-            //Initialize values
-            $cheeses = "";
-            $toppings = "";
-            $sauces = "";
-            
-            $burger = array(
-                'patty' => $burger->patty['type']
-            );
-            $burger['num'] = $burgerCount;
-            
-            // Assign cheeses
-            if (isset($burger->cheeses['top'])) {
-                $cheeses .= $burger->cheeses['top'] . "(top), ";
-            }
-                            
-            if (isset($burger->cheeses['bottom'])) {
-                $cheeses .= $burger->cheeses['bottom'] . "(bottom)";
-            }                
-            $burger['cheese'] = $cheeses;
-            
-            // Assign toppings
-            if (!isset($burger->topping)) {
-                $toppings .= "none";    
-            }                
-            
-            foreach($burger->topping as $topping) {
-                $toppings .= $topping['type'] . ", ";
-            }
-            $burger['toppings'] = $toppings;
-            
-            
-            // Assign sauces
-            if (!isset($burger->sauce)) {
-                $sauces .= "none";    
-            }                
-            
-            foreach($burger->sauce as $sauce) {
-                $sauces .= $sauce['type'] . ", ";
-            }
-            $burger['sauces'] = $sauces;
-            
-            // Assign instructions
-            if (isset($burger->instructions)) {
-                $burger['instructions'] = (string) $burger->instructions;
-            }                
-            else {
-                $burger['instructions'] = "None";
-            }
-                
-            
-            // Assign costs
-            $cost = $this->burgerCost($burger);
-            
-            $burger['cost'] = $cost;
-            $this->total += $cost;       
-            $this->burgers[] = $burger;
-        }
+        $this->load->model('menu');
+        $this->parseXMLOrders();
     }
     
-    // Function used to calculate the cost of each burger
-    // menu.xml file used for pricing of each component
-    private function burgerCost($burger)
+    public function getOrders()
     {
-        $burgerAmount = 0.00;
-        
-        // Calculate patty price
-        $burgerAmount += $this->Menu->getPatty((string) $burger->patty['type'])->price;
-        
-        // Calculate cheese price
-        if (isset($burger->cheeses['top'])) {
-            $burgerAmount += $this->Menu->getCheese((string) $burger->cheeses['top'])->price; 
-        }            
-        
-        if (isset($burger->cheeses['bottom'])) {
-            $burgerAmount += $this->Menu->getCheese((string) $burger->cheeses['bottom'])->price; 
-        }            
-        
-        // Calculate topping price
-        foreach ($burger->topping as $topping) {
-            $burgerAmount += $this->Menu->getTopping((string) $topping['type'])->price; 
+        return $this->orders;
+    }
+    
+    public function getSingleOrder($orderNum)
+    {
+        foreach($this->orders as $order)
+        {
+            if($order['ordernum'] == $orderNum)
+            {
+                return $order;
+            }
         }
         
-        // Calculate sauce price
-        foreach ($burger->sauce as $sauce) {
-            $burgerAmount += $this->Menu->getSauce((string) $sauce['type'])->price; 
+        return NULL;
+    }
+    
+    private function getRawOrders()
+    {
+        $this->load->helper('directory');
+        $dir = directory_map('data', 1, TRUE);
+        
+        $orderFiles = Array();
+        foreach($dir as $d)
+        {
+            if(preg_match('#^order.*.xml$#', $d) == 1)
+            {
+                $orderFiles[] = $d;
+            }
         }
         
-        return $burgerAmount;
+        return $orderFiles;
+    }
+    
+    private function parseXMLOrders()
+    {
+        $rawOrders = $this->getRawOrders();
+        
+        foreach($rawOrders as $r)
+        {
+            $this->xml      = simplexml_load_file(DATAPATH . $r);
+            //$this->orders[] = Array(
+            //    'ordernum'  => pathinfo($r)['filename'],
+            //    'customer'  => (string) $this->xml->customer
+            //);
+            
+            // set up the order
+            $orderTotal     = 0.0;
+            $burgerCount    = 0;
+            $receipt = Array();
+            $receipt['ordernum'] = pathinfo($r)['filename'];
+            $receipt['customer'] = (string) $this->xml->customer;
+            $receipt['type'] = (string)$this->xml['type'];
+            $receipt['burgers'] = Array();
+            $receipt['special'] = '';
+            
+            if(isset($this->xml->special))
+            {
+                $receipt['special'] = 'Special Instructions: ' . (string)$this->xml->special;
+            }
+            
+            foreach($this->xml->burger as $burger)
+            {
+                $burgerTotal = 0.0;
+                
+                $newBurger = Array();
+                $newBurger['burgernum'] = ++$burgerCount;
+                
+                // get patty
+                $patty = $this->menu->getPatty((string)$burger->patty['type']);
+                $newBurger['patty'] = (string)$patty->name;
+                
+                //patty total
+                $burgerTotal += (float)$patty->price;
+                $orderTotal     += (float)$patty->price;
+                
+                // get cheeses
+                $cheeses = '';
+                $cheeseOnTop = false;
+                if(isset($burger->cheeses))
+                {
+                    // check for top cheese
+                    if(isset($burger->cheeses['top']))
+                    {
+                        $cheeseOnTop = true;
+                        $topCheese = $this->menu->getCheese((string)$burger->cheeses['top']);
+                        $cheeses .= (string)$topCheese->name . ' (top)';
+                        
+                        // total top cheese price
+                        $burgerTotal += (float)$topCheese->price;
+                        $orderTotal += (float)$topCheese->price;
+                    }
+                    
+                    // check for bottom cheese
+                    if(isset($burger->cheeses['bottom']))
+                    {
+                        if($cheeseOnTop)
+                        {
+                            $cheeses .= ', ';
+                        }
+                        
+                        $bottomCheese = $this->menu->getCheese((string)$burger->cheeses['bottom']);
+                        $cheeses .= (string)$bottomCheese->name . ' (bottom)';
+                        
+                        // total top cheese price
+                        $burgerTotal += (float)$bottomCheese->price;
+                        $orderTotal += (float)$bottomCheese->price;
+                    }
+                }
+                
+                $newBurger['cheeses'] = $cheeses;
+                
+                // get toppings
+                $toppings = '';
+                $previousTopping = false;
+                if(isset($burger->topping))
+                {
+                    foreach($burger->topping as $t)
+                    {
+                        if($previousTopping)
+                        {
+                            $toppings .= ', ';
+                        }
+                        
+                        $previousTopping = true;
+                        $topping = $this->menu->getTopping((string)$t['type']);
+                        $toppings .= $topping->name;
+                        
+                        // total toppings
+                        $burgerTotal += (float)$topping->price;
+                        $orderTotal += (float)$topping->price;
+                    }
+                }
+                else
+                {
+                    $toppings = 'none';
+                }
+                
+                $newBurger['toppings'] = $toppings;
+                
+                // get sauces
+                $sauces = '';
+                $previousSauce = false;
+                if(isset($burger->sauce))
+                {
+                    foreach($burger->sauce as $s)
+                    {
+                        if($previousSauce)
+                        {
+                            $sauces .= ', ';
+                        }
+                        
+                        $previousSauce = true;
+                        $sauce = $this->menu->getSauce((string)$s['type']);
+                        $sauces .= (string)$sauce->name;
+                    }
+                }
+                else
+                {
+                    $sauces = 'none';
+                }
+                
+                $newBurger['sauces'] = $sauces;
+                
+                // check for instructions
+                $instructions = 'none';
+                if(isset($this->xml->burger->instructions))
+                {
+                    $instructions = (string)$this->xml->burger->instructions;
+                }
+                
+                $newBurger['instructions'] = $instructions; 
+                
+                // final assignments
+                $newBurger['total'] = $burgerTotal;
+                $receipt['burgers'][] = $newBurger;
+            }
+            
+            $receipt['ordertotal'] = $orderTotal;
+            $this->orders[] = $receipt;
+        }
     }
 }
